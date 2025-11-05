d43e3ce5a26c7de4461077c7a3a582bc5c3c8c12
Sniki <igor.prusinowski01@gmail.com>
2025-11-05 20:47:44 +0000

    Add header with styles to lanmding page

--- src/app/components/pages/landing-page/header/header.ts
+++ src/app/components/pages/landing-page/header/header.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { LoginButton } from '../buttons/login-button/login-button';
+import { HomeButton } from '../buttons/home-button/home-button';
+import { ContactButton } from '../buttons/contact-button/contact-button';
+import { AboutUsButton } from '../buttons/about-us-button/about-us-button';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [LoginButton, HomeButton, ContactButton, AboutUsButton],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
