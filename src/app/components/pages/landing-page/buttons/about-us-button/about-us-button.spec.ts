03f9180a5d3dd1a2a724711b8ed3b70c833bf8e6
Sniki <igor.prusinowski01@gmail.com>
2025-11-05 20:30:44 +0000

    Generate landing page components

---
+++ src/app/components/pages/landing-page/buttons/about-us-button/about-us-button.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AboutUsButton } from './about-us-button';
+
+describe('AboutUsButton', () => {
+  let component: AboutUsButton;
+  let fixture: ComponentFixture<AboutUsButton>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AboutUsButton]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AboutUsButton);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/pages/landing-page/buttons/about-us-button/about-us-button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-us-button',
+  imports: [],
+  templateUrl: './about-us-button.html',
+  styleUrl: './about-us-button.css',
+})
+export class AboutUsButton {
+
+}
