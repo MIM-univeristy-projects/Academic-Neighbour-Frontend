9eb1ae56a13fcc9dc7d12e6d9316eb4a3a0c50c6
NaviteLogger <kacprzakmarek92@gmail.com>
2025-11-05 21:00:59 +0000

    Added basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { title: 'Home', path: '', loadComponent: () => import('./components/pages/landing-page/landing-page').then(m => m.LandingPage) },
+    { title: 'Login', path: 'login', loadComponent: () => import('./components/pages/login-page/login-page').then(m => m.LoginPage) },
+    { path: '', redirectTo: 'landing-page', pathMatch: 'full' },
+    { path: '**', redirectTo: 'landing-page', pathMatch: 'full' },
+];
--- src/app/components/pages/landing-page/landing-page.ts
+++ src/app/components/pages/landing-page/landing-page.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { LoginButton } from "./buttons/login-button/login-button";
-import { ContactButton } from "./buttons/contact-button/contact-button";
-import { AboutUsButton } from "./buttons/about-us-button/about-us-button";
-import { HomeButton } from "./buttons/home-button/home-button";
 import { Header } from "./header/header";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [LoginButton, ContactButton, AboutUsButton, HomeButton, Header],
+  imports: [Header],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css',
 })
